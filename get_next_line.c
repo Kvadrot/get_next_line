e0235e09f0f7b6aae369f7b2239246bdf8442266
Illia Tykhonov <send.me.your.maild@gmail.com>
2024-03-28 22:31:26 +0100

    pseudoCode is done, TODO: fix SEGF, mb rewrite strJoin, check up step by step

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:07:43 by itykhono          #+#    #+#             */
-/*   Updated: 2024/03/28 19:09:01 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/03/28 22:26:24 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,68 +17,61 @@ int	ft_strlen(char *str)
 	int len;
 
 	len = 0;
-	while (*str)
+	while (str[len] != '\n')
 	{
 		len++;
 	}
+	len++;
 	return (len);
 }
 
-char	*ft_strdup(const char *s)
+char	*ft_strdup(char *s, int len)
 {
 	char	*newstr;
-	char	*start;
 
-	newstr = (char *)malloc((ft_strlen((char *)s) + 1) * sizeof(char));
+	newstr = (char *)malloc(len * sizeof(char));
 	if (!newstr)
 		return (NULL);
-	start = newstr;
 	while (*s)
 	{
 		*newstr++ = *s++;
 	}
-	*newstr = '\0';
-	return (start);
+	return (newstr);
 }
 
-char	*ft_strjoin(char *s1, char *s2)
+void	*ft_strjoin(char **joined, char *s2, int s1len, int s2len)
 {
 	char	*newstr;
 	int		ind;
 	int		nextInd;
 
-	if (!s1 && s2)
-		return (ft_strdup(s2));
-	else if (s1 && !s2)
-		return (ft_strdup(s1));
-	else if (!s1 && !s2)
-		return ("");
-	newstr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	newstr = (char *)malloc((s1len + s2len) * sizeof(char));
 	if (!newstr)
 		return (NULL);
 	ind = 0;
-	while (s1[ind] != '\0')
+	while (ind <= s1len)
 	{
-		newstr[ind] = s1[ind];
+		newstr[ind] = joined[0][ind];
 		ind ++;
 	}
-	while  (s2[nextInd] != '0')
+	nextInd = 0;
+	while  (nextInd <= s2len)
 	{
 		newstr[ind] = s2[nextInd];
 		ind++;
 		nextInd++;
 	}
-	return (newstr);
+	free(*joined);
+	*joined = ft_strdup(newstr, s1len + s2len);
+	free(newstr);
+	return (NULL);
 }
 
-char	*ft_substr(char *s, unsigned int start, size_t len)
+char	*ft_substr(char *s, int start, int len)
 {
 	char	*substr;
 	int		ind;
-	if (!s)
-		return (NULL);
-	if (start > ft_strlen(s))
-		return (ft_strdup(""));
+
 	if (start + len > ft_strlen(s))
 		len = ft_strlen(s + start);
 	substr = (char *)malloc((len + 1) * sizeof(char));
@@ -91,22 +84,17 @@ char	*ft_substr(char *s, unsigned int start, size_t len)
 		substr[ind] = s[ind + start];
 		ind++;
 	}
-
-	// ft_strlcpy(substr, s + start, len + 1);
 	return (substr);
 }
 
-
-
-int	ft_chrchr(char *str)
+int	ft_strchr(char *str)
 {
 	int ind;
 
 	ind = 0;
-	while ((str[ind] != '\0' || str[ind] != '\n') && ind < BUFFER_SIZE)
+	while (str[ind] != '\n' && ind < BUFFER_SIZE)
 		ind++;
-
-	if (ind < BUFFER_SIZE && (str[ind] != '\0' || str[ind] != '\n'))
+	if (str[ind] != '\n')
 		return (ind);
 	else
 		return (-404);
@@ -115,24 +103,34 @@ int	ft_chrchr(char *str)
 char	*get_next_line(int fd)
 {
 	char		*buffer;
-	char		*small_str;
 	int			bytes_read;
-	static char	*fulltext;
+	char		*new_line;
+	int			new_linelen;
+	static char *rest_text;
+	static int	rest_len;
 
-	buffer = (char *)malloc((BUFFER_SIZE) * sizeof(char));
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	rest_len = 0;
+	buffer = ft_strdup(rest_text, rest_len);
+
+	if (!buffer)
+	{
+		buffer = (char *)malloc((BUFFER_SIZE) * sizeof(char));
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	}
 
     if (bytes_read == -1)
 		return NULL;
-
-	if (ft_chrchr(buffer) >= 0)
+	new_linelen = 0;
+	while (!ft_strchr(buffer))
 	{
-		small_str = ft_substr(buffer, 0, ft_chrchr(buffer));
-		if (!small_str)
-			return (NULL);
-		fulltext = ft_strjoin(fulltext, small_str);
-		return (small_str);
+		new_linelen += bytes_read;
+		ft_strjoin(&new_line, buffer, new_linelen, BUFFER_SIZE);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
 	}
-
-	return (buffer);
+	new_linelen += ft_strchr(buffer);
+	ft_strjoin(&new_line, ft_substr(buffer, 0, ft_strchr(buffer) + 1), new_linelen, BUFFER_SIZE - ft_strchr(buffer));
+	free(rest_text);
+	rest_text = ft_substr(buffer, ft_strchr(buffer) + 1, bytes_read - ft_strchr(buffer));
+	rest_len = bytes_read - ft_strchr(buffer);
+	return (new_line);
 }
--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 int main(int argc, char *argv[])
 {
 	int fd;
-	
     // Open the file for reading
     fd = open("example.txt", O_RDONLY);
 	if (fd == -1) {
         perror("Error opening file");
         exit(EXIT_FAILURE);
     }
+
 	char *test = get_next_line(fd);
 	printf("nextlen = %s", test);
 
